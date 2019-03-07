4fc6d96f147a73093250748c92b003988f39db4b
= <claytonhunt_104@hotmail.com>
2019-03-07 18:09:03 -0500

    added appstate pattern

--- BlazingPizza.Client/Startup.cs
+++ BlazingPizza.Client/Startup.cs
@@ -1,3 +1,4 @@
+using BlazingPizza.Client.AppState;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +8,7 @@ namespace BlazingPizza.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<OrderState>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
